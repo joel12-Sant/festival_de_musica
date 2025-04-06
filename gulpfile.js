d8b149cd51b0257b67c45e3589a3f26aa536b379
joel <joelsantiagos000@gmail.com>
2025-04-06 16:17:14 -0600

    Agregados todos los cambios del scss

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ const sass = gulSass(dartSass);
 
 export function css(done){
     src("src/scss/app.scss")
-        .pipe(sass())
+        .pipe(sass().on("error",sass.logError))
         .pipe(dest("dist/css"));
     done();
 }
 export function dev(){
-    watch("src/scss/app.scss",css);
+    watch("src/scss/**/*.scss",css);
 }
