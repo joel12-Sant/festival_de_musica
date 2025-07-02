4bd52d0a6af2e9f1ff853e66e7170c12ca362395
joel12-Sant <joelsantiagos000@gmail.com>
2025-07-01 21:27:46 -0600

    footer & vis sass navegador

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ import gulSass from "gulp-sass"
 const sass = gulSass(dartSass);
 
 export function css(done){
-    src("src/scss/app.scss")
+    src("src/scss/app.scss",{sourcemaps: true})
         .pipe(sass().on("error",sass.logError))
-        .pipe(dest("dist/css"));
+        .pipe(dest("dist/css", {sourcemaps:true}));
     done();
 }
 export function dev(){
