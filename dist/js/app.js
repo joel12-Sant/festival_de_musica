e55132dfe6e701158d7a29c6d20957d62c28e6aa
joel12-Sant <joelsantiagos000@gmail.com>
2025-08-25 19:49:37 -0600

    aplicando SASS a la galeria

---
+++ dist/js/app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria();
+});
+
+function crearGaleria() {
+    const CANTIDAD_IMAGENES = 16;
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+        const imagen = document.createElement('IMG');
+        imagen.src = `src/img/gallery/full/${i}.jpg`;
+        imagen.alt = `Imagen galeria`;
+        galeria.appendChild(imagen);
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-import {src,dest,watch} from "gulp"
+import {src,dest,watch,series} from "gulp"
 import * as dartSass from "sass" 
 import gulSass from "gulp-sass"
 
 const sass = gulSass(dartSass);
 
+export function js(done){
+    src("src/js/app.js")
+        .pipe(dest("dist/js"));
+    done();
+}
 export function css(done){
     src("src/scss/app.scss",{sourcemaps: true})
         .pipe(sass().on("error",sass.logError))
@@ -12,4 +17,7 @@ export function css(done){
 }
 export function dev(){
     watch("src/scss/**/*.scss",css);
+    watch("src/js/**/*.js",js);
 }
+
+export default series(js,css,dev);
